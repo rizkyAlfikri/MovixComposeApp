cc5ebd28117db6250e476ea1b34ef8d517ca3d6c
rizkyalfikri00 <rizkyalfikri@gmail.com>
2023-04-17 14:53:25 +0700

    Add third party dependencies

---
+++ app/src/main/java/com/example/movixcomposeapp/feature/presentation/theme/Color.kt
@@ -0,0 +1,14 @@
+package com.example.movixcomposeapp.feature.presentation.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFF0D0846)
+val PurpleGrey80 = Color(0xFF0D0846)
+val Pink80 = Color(0xFF0D0846)
+
+val Purple40 = Color(0xFF0D0846)
+val PurpleGrey40 = Color(0xFF0D0846)
+val Pink40 = Color(0xFF0D0846)
+
+val BackgroundColor = Color(0xFFF6F7F9)
+val RattingStartColor = Color(0xFFFBBB00)
